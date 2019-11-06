2071c7a5fbc5c0ae448388fb2d38c66847376825
Wills Blake <richwblake@gmail.com>
2019-11-06 00:56:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect( arr )
+  count = 0
+  new_collection = Array.new
+  
+  while count < arr.length do
+    new_collection.push( yield arr[count] )
+    count += 1
+  end
+  new_collection
+end
